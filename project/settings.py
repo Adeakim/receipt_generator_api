29e196a5ec537509f4b5a187806567098c0e07dc
mac <mac@macs-MacBook-Pro.local>
2022-03-06 23:04:56 +0100

    created models for the roject

--- project/settings.py
+++ project/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "receipt_generator_api"
-    'rest_framework'
+    "receipt_generator_api",
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'receipt_generator_api.User'
